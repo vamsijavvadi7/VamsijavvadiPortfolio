139415843ebecb3d672048f2e353faacb5de3558
Vamsi Javvadi <vamsijavvadi@gmail.com>
2024-09-24 02:36:10 -0400

    all changes

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -2,6 +2,5 @@ const mobileMenu = document.getElementById("mobileMenu");
 const menuToggler = document.getElementById("menuToggler");
 
 menuToggler.addEventListener("click", (e) => {
-  console.log("Menu toggled");
   mobileMenu.classList.toggle("show");
 });
